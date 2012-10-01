30186f15d37c3cf88ba9e539c84aca6d6909c5ba
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2012-10-01 18:12:26 +0200

    TODO 0109

--- tables/package.sql
+++ tables/package.sql
@@ -1,4 +1,5 @@
 create table nd_package (
+-- TODO_0109: why has nd_package no id? Compare with nd_package_body.
   package_name      varchar2(30) primary key,  -- TODO_0095: should probably be nd_plsql_identifier, see nd_package_body.
   invoker_right     varchar2(12) check (invoker_right in ('CURRENT_USER', 'DEFINER')),
   declare_section   references nd_declare_section on delete cascade
